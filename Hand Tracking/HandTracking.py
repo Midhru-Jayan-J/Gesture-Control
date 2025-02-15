26742ae1999399be9cd910d7d9d2040af2e0bdff
Midhru Jayan J <midhru004@gmail.com>
2025-02-15 16:39:41 +0530

    Module implemented for hand tracking

--- Hand Tracking/HandTracking.py
+++ Hand Tracking/HandTracking.py
@@ -8,26 +8,34 @@ import time
 
 cap = cv2.VideoCapture(0) 
 
+mpHands=mp.solutions.hands
 hands = custom_hands.Hands()
+
+pTime=0
+cTime=0
 
 while True:
     success, img = cap.read()
     imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     results=hands.process(imgRGB)
-    mpDraw=mp.solutions.drawing_utils # Draing Line between points
+    mpDraw=mp.solutions.drawing_utils # Drawing the hand points
     
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handLMS in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img,handLMS)
+            mpDraw.draw_landmarks(img,handLMS,mpHands.HAND_CONNECTIONS )# mpHands.HAND_CONNECTIONS-To draw line between connectiong points
             
     
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
     
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_SIMPLEX,3,(255,0,255),3)
     
 
     cv2.imshow("Image", img)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
 
 cap.release()
