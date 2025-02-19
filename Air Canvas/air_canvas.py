0e03b07b6e724173f02a33ab0ac16b5ab49a42af
Midhru Jayan J <midhru004@gmail.com>
2025-02-19 21:26:17 +0530

    Canvas positioning done

--- Air Canvas/air_canvas.py
+++ Air Canvas/air_canvas.py
@@ -6,7 +6,15 @@ cv2.namedWindow("Color detectors")
 
 def setValues(x):
     print("")
-
+    
+# Unlike brighness hue is stable under different lighing conditions
+# Hue - The avtual color
+# Saturation - How vibrant is the color
+# Vibrant - How light or dark the color is 
+ 
+ 
+#cv2.createTrackbar(trackbar_name, window_name, default_value, maximum_value, TrackbarCallback)
+ 
 cv2.namedWindow("Color detectors")
 cv2.createTrackbar("Upper Hue", "Color detectors", 153, 180, setValues)
 cv2.createTrackbar("Upper Saturation", "Color detectors", 255, 255, setValues)
@@ -16,7 +24,69 @@ cv2.createTrackbar("Lower Saturation", "Color detectors", 72, 255, setValues)
 cv2.createTrackbar("Lower Value", "Color detectors", 49, 255, setValues)
 
 
+bpoints=[deque(maxlen=1024)]
+gpoints=[deque(maxlen=1024)]
+rpoints=[deque(maxlen=1024)]
+ypoints=[deque(maxlen=1024)]
+
+blue_index=0
+green_index=0
+red_index=0
+yellow_index=0
+
+# Kernel for dilation 
+kernel = np.ones((5,5),np.uint8)
+
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
+colorIndex = 0
+
+#white canvas
+paintWindow = np.zeros((471, 636, 3)) + 255
+paintWindow = cv2.rectangle(paintWindow, (40,1), (140,65), (0,0,0), 2)
+paintWindow = cv2.rectangle(paintWindow, (160,1), (255,65), colors[0], -1)
+paintWindow = cv2.rectangle(paintWindow, (275,1), (370,65), colors[1], -1)
+paintWindow = cv2.rectangle(paintWindow, (390,1), (485,65), colors[2], -1)
+paintWindow = cv2.rectangle(paintWindow, (505,1), (600,65), colors[3], -1)
+
+cv2.putText(paintWindow, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
+
+
+cap=cv2.VideoCapture(0)
+
 while True:
+    ret,frame=cap.read()
+    frame=cv2.flip(frame,1)
+    hsv=cv2.colorChange(frame,cv2.COLOR_BGR2HSV)
+    
+    u_hue=cv2.getTrackbarPos("Upper Hue","Color Detectors")
+    u_saturation=cv2.getTrackbarPos("Upper Saturation","Color Detectors")
+    u_value = cv2.getTrackbarPos("Upper Value", "Color detectors")
+    l_hue = cv2.getTrackbarPos("Lower Hue", "Color detectors")
+    l_saturation = cv2.getTrackbarPos("Lower Saturation", "Color detectors")
+    l_value = cv2.getTrackbarPos("Lower Value", "Color detectors")
+    
+    Upper_hsv = np.array([u_hue,u_saturation,u_value])
+    Lower_hsv = np.array([l_hue,l_saturation,l_value])
+    
+     # Adding the colour buttons to the live frame for colour access
+    frame = cv2.rectangle(frame, (40,1), (140,65), (122,122,122), -1)
+    frame = cv2.rectangle(frame, (160,1), (255,65), colors[0], -1)
+    frame = cv2.rectangle(frame, (275,1), (370,65), colors[1], -1)
+    frame = cv2.rectangle(frame, (390,1), (485,65), colors[2], -1)
+    frame = cv2.rectangle(frame, (505,1), (600,65), colors[3], -1)
+    cv2.putText(frame, "CLEAR ALL", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+    
+    
+    
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):  
         break
